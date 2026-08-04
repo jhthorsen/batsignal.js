cf9232722a4b647253950fe1b175c374f26b88a7
Jan Henning Thorsen <jan.henning@thorsenlabs.com>
2026-08-04 09:48:45 +0200

    Can count the size of the code
    $ bash count.sh
    13218 # batsignal.js
    4619  # batsignal.js + gzip
    4359  # batsignal.js + brotli
    7802  # batsignal.min.js
    2993  # batsignal.min.js + gzip
    2812  # batsignal.min.js + brotli
    5671  # uglify
    2516  # uglify + gzip
    2338  # uglify + brotli

---
+++ count.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+count() {
+   wc -c | awk '{print $1}';
+}
+
+sed -E '/^[[:space:]]*\/\*/,/^[[:space:]]*\*\//d; /^[[:space:]]*\/\//d; /^[[:space:]]*$/d; s/^[[:space:]]+//; s/[[:space:]]+$//' batsignal.js > batsignal.min.js;
+node --check batsignal.min.js || exit "$?";
+
+echo "$(cat batsignal.js | count) # batsignal.js";
+echo "$(gzip -ck9 batsignal.js | count)  # batsignal.js + gzip";
+echo "$(brotli -ckq 6 batsignal.js | count)  # batsignal.js + brotli";
+
+echo "$(cat batsignal.min.js | count)  # batsignal.min.js";
+echo "$(gzip -ck9 batsignal.min.js | count)  # batsignal.min.js + gzip";
+echo "$(brotli -ckq 6 batsignal.min.js | count)  # batsignal.min.js + brotli";
+
+echo "$(uglifyjs -m properties,toplevel batsignal.js | count)  # uglify";
+echo "$(uglifyjs -m properties,toplevel batsignal.js | gzip -ck9 | count)  # uglify + gzip";
+echo "$(uglifyjs -m properties,toplevel batsignal.js | brotli -ckq 6 | count)  # uglify + brotli";
